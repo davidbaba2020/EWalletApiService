8226d012f385c09579b43a4ba4df4188eda10b9c
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2022-03-10 12:01:04 +0100

    initial setup

---
+++ src/main/java/com/decadave/ewalletapiservice/EWalletApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.decadave.ewalletapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalletApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalletApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decadave/ewalletapiservice/EWalletApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decadave.ewalletapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
